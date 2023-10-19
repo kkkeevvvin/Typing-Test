894d4de375c5dbc4acd71ba33eb83748c91093fb
kkkeevvvin <kevin900726@gmail.com>
2023-10-19 19:55:07 +0800

    add comment, beautify codes, and enable input error state

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,13 @@
 // store the list of words and the index of the word the player is currently typing
 let words = [];
 let wordIndex = 0;
-// the starting time
-let timerStart = false;
-let startTime = Date.now();
 // page elements
 const quoteElement = document.getElementById("quote");
 const messageElement = document.getElementById("message");
 const typedValueElement = document.getElementById("typed-value");
 const restartButton = document.getElementById("restart-button");
 
+// return a random quote
 function randQuote() {
   // all of our quotes
   const quotes = [
@@ -48,61 +46,71 @@ const gameSetUp = function () {
   messageElement.innerText = "";
 
   // Setup the textbox
-  // Clear the textbox
+  // Enable the textbox input
   typedValueElement.disabled = false;
+  // Clear the textbox
   typedValueElement.value = "";
   // set focus
   typedValueElement.focus();
 };
 
+// Initial game setup
 gameSetUp();
 
-// fuctions here
+// Set restart button to func gameSetUp
 restartButton.addEventListener("click", gameSetUp);
 
-typedValueElement.addEventListener("input", () => {
-  // Get the current word
-  const currentWord = words[wordIndex];
-  // get the current value
-  const typedValue = typedValueElement.value;
+// Process Textbox Input
+(function () {
+  let timerState = false;
+  let startTime;
 
-  if (typedValue && !timerStart) {
-    console.log("start timer!");
-    timerStart = true;
-    startTime = new Date().getTime();
-  }
+  typedValueElement.addEventListener("input", () => {
+    // Get the current word
+    const currentWord = words[wordIndex];
+    // get the current value
+    const typedValue = typedValueElement.value;
 
-  if (typedValue === currentWord && wordIndex === words.length - 1) {
-    // end of sentence
-    // Display success
-    timerStart = false;
-    typedValueElement.disabled = true;
-    typedValueElement.value = "";
-    quoteElement.childNodes[wordIndex].className = "";
-    const elapsedTime = new Date().getTime() - startTime;
-    const message = `CONGRATULATIONS! You finished in ${
-      elapsedTime / 1000
-    } seconds.`;
-    messageElement.innerText = message;
-  } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
-    // end of word
-    // clear the typedValueElement for the new word
-    typedValueElement.value = "";
-    quoteElement.childNodes[wordIndex].className = "";
-    // move to the next word
-    wordIndex++;
-    // reset the class name for all elements in quote
-    // for (const wordElement of quoteElement.childNodes) {
-    //   wordElement.className = "";
-    // }
-    // highlight the new word
-    quoteElement.childNodes[wordIndex].className = "highlight";
-  } else if (currentWord.startsWith(typedValue)) {
-    // currently correct
-    // highlight the next word
-    typedValueElement.className = "";
-  } else {
-    // error state
-    typedValueElement.className = "error";
-  }
-});
+    if (typedValue && !timerState) {
+      timerState = true;
+      startTime = new Date().getTime();
+    }
+
+    if (typedValue === currentWord && wordIndex === words.length - 1) {
+      // end of sentence
+      // reset timer state
+      timerState = false;
+      // reset last word state
+      quoteElement.childNodes[wordIndex].className = "";
+      // disable textbox input
+      typedValueElement.disabled = true;
+      // clear textbox
+      typedValueElement.value = "";
+
+      // Display success
+      const elapsedTime = new Date().getTime() - startTime;
+      const message = `CONGRATULATIONS! You finished in ${
+        elapsedTime / 1000
+      } seconds.`;
+      messageElement.innerText = message;
+    } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
+      // end of word
+      // reset the old word state to none
+      quoteElement.childNodes[wordIndex].className = "";
+      // clear the typedValueElement for the new word
+      typedValueElement.value = "";
+      // move to the next word
+      wordIndex++;
+      // highlight the new word
+      quoteElement.childNodes[wordIndex].className = "highlight";
+    } else if (currentWord.startsWith(typedValue)) {
+      // currently correct
+      // highlight the next word
+      typedValueElement.className = "";
+      quoteElement.childNodes[wordIndex].className = "highlight";
+    } else {
+      // error state
+      quoteElement.childNodes[wordIndex].className = "error";
+    }
+  });
+})();
