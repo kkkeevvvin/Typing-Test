708aa043fcf2e1c4327f94b98f72eeb74ea5cc5a
kkkeevvvin <kevin900726@gmail.com>
2023-10-19 10:08:44 +0800

    revise ghpage-upadate.sh

--- ghpage-upadate.sh
+++ ghpage-upadate.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+echo "git checkout gh-pages"
 git checkout gh-pages
+
+echo "git merge main"
 git merge main
+
+echo "git push origin gh-pages"
 git push origin gh-pages
+
+echo "git checkout main"
 git checkout main
